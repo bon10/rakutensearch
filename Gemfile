2edb4066fb1bd286156714871896f92433660a33
bon User <bon@v5560.vir.kagoya.net>
2016-04-08 17:11:50 +0900

    なんかできた

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem "sinatra-cross_origin"
+
+group :development do
+  gem 'foreman'
+end
--- app.rb
+++ app.rb
@@ -1,10 +1,34 @@
 require 'sinatra'
+require 'sinatra/cross_origin'
+require 'logger'
+require 'net/http'
+
+configure do
+  logger = Logger.new('sinatra.log')
+  enable :cross_origin
+end
 
 get '/' do
-  "the time where this server lives is #{Time.now}
-    <br /><br />check out your <a href=\"/agent\">user_agent</a>"
+  "pong"
 end
 
-get '/agent' do
-  "you're using #{request.user_agent}"
+post '/search' do
+  uri = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20140222"
+  res = Net::HTTP.post_form((URI.parse uri),
+    {'applicationId': ENV["applicationId"],
+     'affiliateId': "0641f941.17ca8956.0641f942.e1c0fb28",
+     'keyword': params["keyword"],
+     'imageFlg': params["imageFlg"],
+     'availability': params["availability"],
+     'sort': params["sort"],
+     'pointRateFlag': params["pointRateFlag"],
+     'pointRate': params["pointRate"],
+     'creditCardFlag': params["creditCardFlag"],
+     'page': params["page"]
+     }
+  )
+  #logger.info res.body
+  return res.body
 end
+
+
